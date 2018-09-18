62b78db92ef2d4041216afe60070be94351a9735
angiecheng <angiesc2@illinois.edu>
2018-09-18 18:24:15 -0500

    Word Counter Function added

--- src/WebScraper.java
+++ src/WebScraper.java
@@ -1,5 +1,39 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
 public class WebScraper {
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+    public static int wordCount(String urlToString) {
+        int numberofWords = 0;
+        for (int i = 0; i < urlToString.length(); i++) {
+            if (urlToString.charAt(i) != ' ') {
+                numberofWords++;
+                while(urlToString.charAt(i) != ' ' && i < urlToString.length() - 1) {
+                    i++;
+                }
+            }
+        }
+        return numberofWords;
+    }
+
     public static void main(String[] unused) {
-        System.out.println("Hello World");
+        System.out.print("Word count is: " + wordCount(urlToString("http://erdani.com/tdpl/hamlet.txt")));
     }
 }
